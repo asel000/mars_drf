11dc3b1dd3482dd21d0dec851a02af05539374a3
Your Name <you@example.com>
2022-04-26 15:05:11 +0600

    first

--- post/api/views.py
+++ post/api/views.py
@@ -1,30 +1,69 @@
-from rest_framework.generics import CreateAPIView, ListAPIView,RetrieveAPIView
-from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.mixins import UpdateModelMixin, DestroyModelMixin
 from rest_framework.views import APIView
-
+from rest_framework import permissions
+from accounts.api.permissions import IsOwnerOrReadOnly
 from post.api.serializers import PostSerializer
 from post.models import Post
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 
-class PostAPIView(APIView):
-    permission_classes = []
-    authentication_classes = []
+class PostListAPIView(ListAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # authentication_classes = []
+    serializer_class = PostSerializer
 
-
-    def get(self, request, *args, **kwargs):
+    def get_queryset(self):
         qs = Post.objects.all()
-        serializers = PostSerializer(qs, many=True)
-        return Response(serializers.data) #post add
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(content__icontains=query)
+        return qs
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class PostCreateAPIView(CreateAPIView):
-    permission_classes = []
-    authentication_classes = []
+    permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = []
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetailAPIView(RetrieveAPIView):
-    permission_classes = []
-    authentication_classes = []
+class PostDetailAPIView(RetrieveAPIView, UpdateModelMixin, DestroyModelMixin):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    # authentication_classes = [SessionAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class PostUpdateAPIView(UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated,IsOwnerOrReadOnly]
+    # authentication_classes = [SessionAuthentication]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
+
+
+class PostDeleteAPIView(DestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated,IsOwnerOrReadOnly]
+    # authentication_classes = [SessionAuthentication]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
--- post/models.py
+++ post/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.conf import settings
 
 
+
+def upload_post_image(inctance,filename):
+    return 'post/{user}/{filename}'.format(user=inctance.user,filename=filename)
+
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='post_image',null=True,blank=True)
 
     def __str__(self):
         return self.title
